29f6946fef9946a384df806a13f02d281bb844c5
palevoo <kud.nikita@gmail.com>
2018-01-16 23:32:51 +0100

    feat: footer added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './infinity-symbol.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Entrance from './components/entrance/entrance.js';
 import Video from './components/video/video.js';
 import Recording from './components/recording/recording.js';
@@ -21,7 +20,7 @@ class App extends Component {
               <h1 className="App-title">Chronos.io</h1>
           </header>
           <Router>
-            <div>
+            <div className="main">
               <Switch>
                 <Route exact path="/" component={Entrance}/>
                 <Route path="/video" component={Video}/>
@@ -32,6 +31,9 @@ class App extends Component {
               </Switch>
             </div>
           </Router>
+          <footer className="App-footer">        
+            <a class="github-button" href="https://github.com/palevoo/chronos.io/issues" data-show-count="true" aria-label="Issue palevoo/chronos.io on GitHub">Issue</a>
+          </footer>
         </div>
     );
   }
--- src/components/entrance/entrance.js
+++ src/components/entrance/entrance.js
@@ -8,7 +8,7 @@ class Entrance extends Component {
     return (
       <div className="Entrance, container">
         <h2>Welcome to the Chronos.io, a time travel app</h2>
-        <p>Designed in a decentralized manner, allowing you to record video message, upload it to IPFS</p>
+        <p>Designed in a decentralized manner, allowing you to record a video message, and upload it to the IPFS</p>
         <p>Ethereum smart contract will make sure, that you cannot retrieve it before the teleportation date.</p>
         <Link to="/video">
           <button id="enter">Enter</button>
